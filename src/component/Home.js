4d03d7428d7ec14245219da1a98ab79feba7bdd6
Cody <bcuy46@yahoo.com>
2023-06-07 22:56:05 -0400

    Updated home component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,57 +1,61 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import GameCard from './GameCard';
+import Button from '@mui/material/Button';
+import { alpha, styled, useTheme } from '@mui/material/styles';
 
 const genres = [
-    { name: 'Action', value: 'action' },
-    { name: 'Adventure', value: 'adventure' },
-    { name: 'Indie', value: 'indie' },
+  { name: 'Action', value: 'action' },
+  { name: 'Adventure', value: 'adventure' },
+  { name: 'Indie', value: 'indie' },
     { name: 'RPG', value: 'rpg' },
-      { name: 'Simulation', value: 'simulation' },
-      { name: 'Sports', value: 'sports' },
-      { name: 'Strategy', value: 'strategy' },
-      { name: 'Casual', value: 'casual' },
-      { name: 'Arcade', value: 'arcade' },
-      { name: 'Puzzle', value: 'puzzle' },
-      { name: 'Platformer', value: 'platformer' },
-      { name: 'Racing', value: 'racing' },
-      { name: 'Shooter', value: 'shooter' },
-      { name: 'Fighting', value: 'fighting' },
-      { name: 'Family', value: 'family' },
-      { name: 'Board Games', value: 'board-games' },
-      { name: 'Educational', value: 'educational' },
-      { name: 'Card', value: 'card' },
-      { name: 'Massively Multiplayer', value: 'massively-multiplayer' },
+    { name: 'Simulation', value: 'simulation' },
+    { name: 'Sports', value: 'sports' },
+    { name: 'Strategy', value: 'strategy' },
+    { name: 'Casual', value: 'casual' },
+    { name: 'Arcade', value: 'arcade' },
+    { name: 'Puzzle', value: 'puzzle' },
+    { name: 'Racing', value: 'racing' },
+    { name: 'Shooter', value: 'shooter' },
+    { name: 'Fighting', value: 'fighting' },
+    { name: 'Platformer', value: 'platformer' },
+    { name: 'Massively Multiplayer', value: 'massively-multiplayer' },
+    { name: 'Family', value: 'family' },
+    { name: 'Board Games', value: 'board-games' },
+    { name: 'Educational', value: 'educational' },
+    { name: 'Card', value: 'card' },
 ];
 
+const GenreButton = styled(Button)(({ theme }) => ({
+  borderRadius: theme.shape.borderRadius,
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+}));
+
 const Home = () => {
-  const [selectedGenre, setSelectedGenre] = React.useState('action');
+  const theme = useTheme();
 
   return (
     <div>
-      <div>
-        {genres.map((genre) => (
-          <Button
-            key={genre.value}
-            onClick={() => setSelectedGenre(genre.value)}
-            sx={{
-              backgroundColor: selectedGenre === genre.value ? 'Grey' : 'white',
-              color: selectedGenre === genre.value ? 'white' : 'black',
-              borderRadius: '50px',
-              padding: '10px 20px',
-              marginRight: '10px',
-              '&:hover': {
-                backgroundColor: 'lightGrey',
-              },
-            }}
-          >
-            {genre.name}
-          </Button>
-        ))}
-      </div>
+      {genres.map((genre) => (
+        <GenreButton
+          key={genre.value}
+          component="a"
+          href={`/${genre.value}`}
+          sx={{
+            backgroundColor: theme.palette.background.default,
+            color: theme.palette.text.primary,
+            '&:hover': {
+              backgroundColor: alpha(theme.palette.common.white, 0.25),
+            },
+          }}
+        >
+          {genre.name}
+        </GenreButton>
+      ))}
       <GameCard />
     </div>
   );
-}
+};
 
 export default Home;
